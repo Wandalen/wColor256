02c6451e8fa7560b5ecb1e18c2913a7ff981f9a5
wandalen <wandalen.me@gmail.com>
2016-11-24 16:35:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wColor256
+Module in JavaScript providing convenient means for color conversion. Aware about 256 color names.
---
+++ staging/amid/color/Color256.s
@@ -0,0 +1,59 @@
+(function _Color256_s_() {
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+
+  if( typeof wBase === 'undefined' )
+  try
+  {
+    require( '../wTools.s' );
+  }
+  catch( err )
+  {
+    require( 'wTools' );
+  }
+
+  requre( 'wColor' );
+
+}
+
+var _ = wTools;
+var Self = wTools;
+
+// --
+// var
+// --
+
+var ColorMap =
+{
+
+  'invisible'   : [ 0.0,0.0,0.0,0.0 ],
+  'transparent' : [ 1.0,1.0,1.0,0.5 ],
+  'white'       : [ 1.0,1.0,1.0 ],
+  'black'       : [ 0.0,0.0,0.0 ],
+  'red'         : [ 1.0,0.0,0.0 ],
+  'green'       : [ 0.0,1.0,0.0 ],
+  'blue'        : [ 0.0,0.0,1.0 ],
+  'yellow'      : [ 1.0,1.0,0.0 ],
+
+}
+
+// --
+// prototype
+// --
+
+var Proto =
+{
+
+  // var
+
+  ColorMap : ColorMap,
+
+}
+
+_.mapSupplement( wTools,Proto );
+_.mapSupplement( wTools.ColorMap,ColorMap );
+
+})();
