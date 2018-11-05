1306aa5f7cd22a67274548cc02146f262c32b8ba
Wandalen <wandalen.me@gmail.com>
2018-11-05 17:15:14 +0200

    auto

--- proto/dwtools/amid/color/Color256.s
+++ proto/dwtools/amid/color/Color256.s
@@ -357,7 +357,7 @@ else
 
 if( typeof module !== 'undefined' )
 if( _global_.WTOOLS_PRIVATE )
-delete require.cache[ module.id ];
+{ /* delete require.cache[ module.id ]; */ }
 
 if( typeof module !== 'undefined' && module !== null )
 module[ 'exports' ] = Self;
