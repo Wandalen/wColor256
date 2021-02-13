c31721b38cfb482162846237c8a5775d03210eba
wandalen <wandalen.me@gmail.com>
2021-02-13 09:20:11 +0200

    .

--- proto/wtools/amid/l1/color/l5/Color256.s
+++ proto/wtools/amid/l1/color/l5/Color256.s
@@ -2,17 +2,6 @@
 
 'use strict';
 
-/**
- * Collection of cross-platform routines to operate colors conveniently. Extends basic implementation Color by additional color names. Color provides functions to convert color from one color space to another color space, from name to color and from color to the closest name of a color. The module does not introduce any specific storage format of color what is a benefit. Color has a short list of the most common colors. Use the module for formatted colorful output or other sophisticated operations with colors.
-  @module Tools/mid/Color256
-*/
-
-if( typeof module !== 'undefined' )
-{
-  let _ = require( '../../../wtools/Tools.s' );
-  _.include( 'wColor' );
-}
-
 let _ = _global_.wTools;
 let Self = _.color = _.color || Object.create( null );
 
